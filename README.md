ffbad279dcbb6bf196b87d01f7bd731b7bbcb6d9
Reggie S <reggie@bgcreatives.com>
2016-02-27 03:21:14 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,34 @@
+﻿#Readme
+
+This is the handlebars version of FreeCodeCamp Zipline project "[Twitch JSON API](http://www.freecodecamp.com/challenges/use-the-twitchtv-json-api)". Follow the link
+to see all user stories for the [project](http://www.freecodecamp.com/challenges/use-the-twitchtv-json-api).
+My first version of the Twitch JSON API is at [codepen](http://codepen.io/Reggie01/full/WwNbjg/) and source code [github](https://github.com/Reggie01/Zipline/tree/master/Zipline/TwitchtvJSONAPI).
+
+### Technologies Used
+* javascript
+* html
+* css
+* jquery
+* bootstrap
+* handlebars
+
+### User Stories
+* I can search Wikipedia entries in a search box and see the resulting Wikipedia entries.
+* I can click a button to see a random Wikipedia entry.
+
+
+### Components
+  * Link/Button for a random Wikipedia entry
+  * search box for Wikipedia entries
+  * list items for Wikipedia entries
+  
+### APIs
+* https://www.mediawiki.org/wiki/API:Main_page
+
+### Technologies Used
+* javascript
+* html
+* css
+* jquery
+* bootstrap
+* handlebars
---
+++ js/index.js
@@ -0,0 +1,53 @@
+﻿$(document).ready( function() {
+  
+  // Comiple handlbars template
+  var listComponent   = $("#list-template").html();
+  var listTemplate = Handlebars.compile( listComponent );
+  
+  var searchComponent = $("#searchbox-template").html();
+  var searchTemplate = Handlebars.compile( searchComponent );
+  
+  var randomWikiComponent = $("#random-article-template").html();
+  var randomWikiTemplate = Handlebars.compile( randomWikiComponent );
+  
+  $("#random_article_container").html(randomWikiTemplate);
+  $("#searchbox_container").html(searchTemplate);
+  $("#list_ul").html(listTemplate);
+  
+  function handleWikipediaResp( data ){
+     console.log( data );
+  }
+  
+  $.ajaxSetup({
+        type : "GET",
+        dataType : "jsonp",
+    });
+    
+    function AjaxErrorHandler( xhr, status, errorThrown ){
+      console.log("Sorry there was a problem.");
+      console.log("Error: " + errorThrown);
+      console.log("Status: " + status);
+      console.dir(xhr);
+    }
+    
+   function wikiAjaxRequest(url, options) {
+       return $.ajax({
+                    url : url,
+                    jsonp : "callback",
+                    data: options,
+                    headers: { 'Api-User-Agent': "http://codepen.io/Reggie01; reggie@bgcreatives.com" },
+                  });
+    };
+    
+    function ajaxRequest() {
+      var options = {
+         action: "query",
+         format: "json",
+      }
+      var request = wikiAjaxRequest("https://en.wikipedia.org/w/api.php?action=query&titles=Javascript&prop=revisions&rvprop=content&format=json");
+      request.then(handleWikipediaResp).
+        fail( AjaxErrorHandler );
+    };
+    
+    ajaxRequest();
+});
